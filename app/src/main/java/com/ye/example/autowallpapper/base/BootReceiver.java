56e5ab7a4054f4dcb61e17414e292faed44f7aa6
zhihao <zhihao_pp@163.com>
2019-08-05 14:33:55 +0800

    添加：开机启动

---
+++ app/src/main/java/com/ye/example/autowallpapper/base/BootReceiver.java
@@ -0,0 +1,17 @@
+package com.ye.example.autowallpapper.base;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.ye.example.autowallpapper.common.Initializer;
+
+/**
+ * @author yezhihao 2019-08-05 14:30
+ */
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Initializer.init(context);
+    }
+}
